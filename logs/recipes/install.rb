19f233b795a0fb61bc88641ed767617050668a82
Mohammed O. Tillawy <tillawy@gmail.com>
2015-12-10 21:52:36 +0200

    change region

--- logs/recipes/install.rb
+++ logs/recipes/install.rb
@@ -8,6 +8,6 @@ remote_file "/opt/aws/cloudwatch/awslogs-agent-setup.py" do
 end
 
 execute "Install CloudWatch Logs agent" do
-  command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r us-east-1 -c /tmp/cwlogs.cfg"
+  command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r us-west-2 -c /tmp/cwlogs.cfg"
   not_if { system "pgrep -f aws-logs-agent-setup" }
 end
